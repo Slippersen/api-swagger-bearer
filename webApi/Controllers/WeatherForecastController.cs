37843181edcf4b9a85f9b90c3a814a9c1217a6f4
Erik J. Sandal <erik.sandal@bouvet.no>
2020-11-11 11:40:17 +0100

    Included Swashbuckle

--- webApi/Controllers/WeatherForecastController.cs
+++ webApi/Controllers/WeatherForecastController.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace webApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Produces("application/json")]
+    [SwaggerTag("Example API controller for weather forecasts")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -23,7 +26,13 @@ namespace webApi.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Returns random weather forecasts.
+        /// </summary>
+        /// <returns>Random weather forecasts</returns>
+        /// <response code="200">Success</response>
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<WeatherForecast>), StatusCodes.Status200OK)]
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
--- webApi/Startup.cs
+++ webApi/Startup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Reflection;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace webApi
 {
@@ -26,6 +29,34 @@ namespace webApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "webApi",
+                    Description = "A simple example ASP.NET Core Web API with Swagger and Bearer auth",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Firstname Lastname",
+                        Email = "name@website.net",
+                        Url = new Uri("https://website.net"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Example license",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +66,19 @@ namespace webApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "webApi V1");
+
+                // serve the Swagger UI at the app's root (http://localhost:<port>/)
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
